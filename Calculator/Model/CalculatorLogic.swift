f514ed7ee327188efe4e85a9cb3a8f1b72c71d00
Argenis De La Rosa <hennyrosexo@gmail.com>
2020-03-09 13:51:52 -0400

    refactored the if let's into switch statements and setup the final calculation methods

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,22 +12,52 @@ struct CalculatorLogic {
     
     private var number: Double?
     
+    private var intermediateCalculation: (number1: Double, calculationMethod: String)?
+    
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         
-        if symbol == "+/-" {
-            return number * -1
-        } else if symbol == "AC" {
-            return 0
-        } else if symbol == "%" {
-            return number * 0.01
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumberCalculation(number2: n)
+            default:
+                intermediateCalculation = (number1: n, calculationMethod: symbol)
+            }
         }
-        
         return nil
         
+    }
+    
+    func performTwoNumberCalculation(number2: Double) -> Double? {
+        
+        if let number1 = intermediateCalculation?.number1,
+            let operation = intermediateCalculation?.calculationMethod {
+            
+  
+            switch operation {
+            case "+":
+                return number1 + number2
+            case "-":
+                return number1 - number2
+            case "×":
+                return number1 * number2
+            case "÷":
+                return number1 / number2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        return nil
     }
     
 }
